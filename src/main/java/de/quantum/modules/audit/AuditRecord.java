d4555c5d9e8360a57031a964916016ca47a82f48
Kyle <lelegrau04@gmail.com>
2024-08-10 20:30:03 +0200

    .gitignore update, archiving files

--- src/main/java/de/quantum/modules/audit/AuditRecord.java
+++
@@ -1,6 +0,0 @@
-package de.quantum.modules.audit;
-
-import net.dv8tion.jda.api.audit.ActionType;
-
-public record AuditRecord(long qid, String userId, String targetId, int rawActionType, String action) {
-}
