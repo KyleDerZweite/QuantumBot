69977e28ab468b6b2623b33dcc85a00b40e51d97
Kyle <lelegrau04@gmail.com>
2024-08-28 20:36:56 +0200

    CustomBot alpha

--- src/main/java/de/quantum/modules/custombot/CustomBotDatabaseManager.java
+++ src/main/java/de/quantum/modules/custombot/CustomBotDatabaseManager.java
@@ -4,9 +4,12 @@ import de.quantum.core.BotType;
 import de.quantum.core.database.DatabaseManager;
 import de.quantum.core.utils.CheckUtils;
 import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.entities.Activity;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 @Slf4j
 public class CustomBotDatabaseManager {
@@ -22,6 +25,9 @@ public class CustomBotDatabaseManager {
             String createTableQuery = "CREATE TABLE IF NOT EXISTS " + DATABASE_NAME + " (" +
                     "bot_id VARCHAR(32), " +
                     "guild_id VARCHAR(32), " +
+                    "activity_type_id INTEGER, " +
+                    "activity_name VARCHAR(128), " +
+                    "activity_url VARCHAR(128), " +
                     "active BOOLEAN, " +
                     "verified BOOLEAN " +
                     ")";
@@ -30,24 +36,31 @@ public class CustomBotDatabaseManager {
             statement.close();
         } catch (SQLException e) {
             log.error(e.getMessage());
-            return false;
+            return true;
         }
-        return true;
+        return false;
     }
 
     public static void addCustomBot(@NotNull String botId, @NotNull String encryptedToken, String guildId) {
-        addCustomBot(botId, encryptedToken, guildId, true, true);
+        addCustomBot(botId, encryptedToken, guildId, 0, "",null, true, true);
     }
 
-    public static void addCustomBot(@NotNull String botId, @NotNull String encryptedToken, String guildId, boolean isActive, boolean isVerified) {
-        if (!ensureTableExists()) return;
+    public static void addCustomBot(
+            @NotNull String botId, @NotNull String encryptedToken,
+            String guildId, int activityTypeId, String activityName,
+            @Nullable String activityUrl,
+            boolean isActive, boolean isVerified) {
+        if (ensureTableExists()) return;
         DatabaseManager.getInstance().insertNewBot(botId, encryptedToken, BotType.CUSTOM);
-        String sql = "INSERT INTO " + DATABASE_NAME + " (bot_id, guild_id, active, verified) VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO " + DATABASE_NAME + " (bot_id, guild_id, activity_type_id, activity_name, activity_url, active, verified) VALUES (?, ?, ?, ?, ?, ?, ?)";
         try (PreparedStatement stmt = DatabaseManager.getInstance().getConnection().prepareStatement(sql)) {
             stmt.setString(1, botId);
             stmt.setString(2, guildId);
-            stmt.setBoolean(3, isActive);
-            stmt.setBoolean(3, isVerified);
+            stmt.setInt(3, activityTypeId);
+            stmt.setString(4, activityName);
+            stmt.setString(5, activityUrl);
+            stmt.setBoolean(6, isActive);
+            stmt.setBoolean(7, isVerified);
             stmt.executeUpdate();
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
@@ -55,6 +68,7 @@ public class CustomBotDatabaseManager {
     }
 
     public static boolean guildHasCustomBot(@NotNull String guildId) {
+        if (ensureTableExists()) return false;
         ResultSet rs = DatabaseManager.getInstance().selectFromWhere("*", DATABASE_NAME, "guild_id", guildId);
         if (CheckUtils.checkNull(rs)) {
             return false;
@@ -70,6 +84,7 @@ public class CustomBotDatabaseManager {
     }
 
     public static boolean isCustomBot(String botId) {
+        if (ensureTableExists()) return false;
         ResultSet rs = DatabaseManager.getInstance().selectFromWhere("*", DATABASE_NAME, "bot_id", botId);
         if (CheckUtils.checkNull(rs)) {
             return false;
@@ -84,4 +99,93 @@ public class CustomBotDatabaseManager {
         return false;
     }
 
+    public static void updateActivity(String botId, int activityTypeId, String activityName, String activityUrl) {
+        if (ensureTableExists()) return;
+        String sql = "UPDATE " + DATABASE_NAME + " SET activity_type_id = ?, activity_name = ?, activity_url = ? WHERE bot_id = ?";
+        try (PreparedStatement stmt = DatabaseManager.getInstance().getConnection().prepareStatement(sql)) {
+            stmt.setInt(1, activityTypeId);
+            stmt.setString(2, activityName);
+            stmt.setString(3, activityUrl);
+            stmt.setString(4, botId);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    public static Activity getCustomBotActivity(@NotNull String botId) {
+        if (ensureTableExists()) return null;
+        ResultSet rs = DatabaseManager.getInstance().selectFromWhere("activity_type_id, activity_name, activity_url", DATABASE_NAME, "bot_id", botId);
+        if (CheckUtils.checkNull(rs)) {
+            return null;
+        }
+        try {
+            if (rs.next()) {
+                int activityTypeId = rs.getInt("activity_type_id");
+                String activityName = rs.getString("activity_name");
+                String activityUrl = rs.getString("activity_url");
+                return Activity.of(Activity.ActivityType.fromKey(activityTypeId), activityName, activityUrl);
+            }
+        } catch (SQLException e) {
+            log.debug(e.getMessage(), e);
+        }
+        return null;
+    }
+
+    @NotNull
+    public static ArrayList<String> getCustomBotTokens() {
+        ArrayList<String> customBotTokens = new ArrayList<>();
+        if (ensureTableExists()) return customBotTokens;
+        ResultSet rs = DatabaseManager.getInstance().selectFrom("*", DATABASE_NAME);
+        if (CheckUtils.checkNull(rs)) {
+            return customBotTokens;
+        }
+
+        try {
+            while (rs.next()) {
+                String botId = rs.getString("bot_id");
+                boolean isActive = rs.getBoolean("active");
+                boolean isVerified = rs.getBoolean("verified");
+                if (!isActive || !isVerified) continue;
+                ResultSet botTokenSet = DatabaseManager.getInstance().selectFromWhere("token", DatabaseManager.BOT_TABLE_NAME, "bot_id", botId);
+                if (botTokenSet.next()) {
+                    String botToken = botTokenSet.getString("token");
+                    customBotTokens.add(botToken);
+                }
+                botTokenSet.close();
+            }
+            rs.close();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+        }
+        return customBotTokens;
+    }
+
+    public static String getCustomBotToken(@NotNull String botId) {
+        if (ensureTableExists()) return null;
+        ResultSet rs = DatabaseManager.getInstance().selectFromWhere("active, verified", DATABASE_NAME, "bot_id", botId);
+        if (CheckUtils.checkNull(rs)) {
+            return null;
+        }
+        try {
+            if (rs.next()) {
+                boolean isActive = rs.getBoolean("active");
+                boolean isVerified = rs.getBoolean("verified");
+                if (!isActive || !isVerified) return null;
+                ResultSet botTokenSet = DatabaseManager.getInstance().selectFromWhere("token", DatabaseManager.BOT_TABLE_NAME, "bot_id", botId);
+                if (botTokenSet.next()) {
+                    String token = botTokenSet.getString("token");
+                    rs.close();
+                    botTokenSet.close();
+                    return token;
+                }
+                botTokenSet.close();
+            }
+            rs.close();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+        }
+        return null;
+    }
 }
+
--- src/main/java/de/quantum/modules/custombot/CustomBotManager.java
+++ src/main/java/de/quantum/modules/custombot/CustomBotManager.java
@@ -2,16 +2,25 @@ package de.quantum.modules.custombot;
 
 import de.quantum.core.events.EventReflector;
 import de.quantum.core.module.ModuleAnnotation;
+import de.quantum.core.shutdown.ShutdownAnnotation;
+import de.quantum.core.shutdown.ShutdownInterface;
+import de.quantum.core.utils.Secret;
+import de.quantum.core.utils.Utils;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.ConcurrentSessionController;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 import java.util.EnumSet;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 
+@Slf4j
 @ModuleAnnotation(
         moduleName = "CustomBot",
         moduleDescription = "Starting another Discord Bot with specific Token, which has the same functionality as Quantum.",
@@ -19,8 +28,12 @@ import java.util.concurrent.Executors;
         moduleAuthorName = "kylederzweite",
         moduleAuthorID = "378542649579143188"
 )
-public class CustomBotManager {
+@ShutdownAnnotation
+public class CustomBotManager implements ShutdownInterface {
     private static volatile CustomBotManager INSTANCE = null;
+
+    @Getter
+    private final ConcurrentHashMap<String, JDA> customJdaInstanceMap;
 
     private CustomBotManager() {
         if (INSTANCE != null) {
@@ -29,6 +42,7 @@ public class CustomBotManager {
                             + CustomBotManager.class.getName()
                             + " class already exists, Can't create a new instance.");
         }
+        this.customJdaInstanceMap = new ConcurrentHashMap<>();
     }
 
     public static CustomBotManager getInstance() {
@@ -42,15 +56,75 @@ public class CustomBotManager {
         return INSTANCE;
     }
 
+    public void startBot(String botId) {
+        stopBot(botId);
+        String token = CustomBotDatabaseManager.getCustomBotToken(botId);
+        if (token == null) {
+            return;
+        }
+        JDA jda = getCustomBotJda(token);
+        customJdaInstanceMap.put(botId, jda);
+        updateBotActivity(jda.getSelfUser().getId());
+    }
+
+    public void stopBot(String botId) {
+        JDA botJda = customJdaInstanceMap.getOrDefault(botId, null);
+        if (botJda != null) {
+            log.warn("Shutting down {}", Utils.getJdaShardGuildCountString(botJda));
+            botJda.shutdown();
+            botJda.shutdownNow();
+        }
+    }
+
+    public JDA getTestCustomBotJDA(String token) {
+        token = Secret.decrypt(token);
+        return JDABuilder.createLight(token).build();
+    }
+
     public JDA getCustomBotJda(String token) {
+        token = Secret.decrypt(token);
         return JDABuilder.create(token, EnumSet.allOf(GatewayIntent.class))
                 .setStatus(OnlineStatus.DO_NOT_DISTURB)
                 .enableCache(EnumSet.allOf(CacheFlag.class))
                 .setSessionController(new ConcurrentSessionController())
-                .setCallbackPool(Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()), true)
+                .setCallbackPool(Executors.newFixedThreadPool(1), true)
                 .setBulkDeleteSplittingEnabled(false)
                 .setRawEventsEnabled(true)
                 .addEventListeners(new EventReflector()).build();
     }
 
+    public void startAll() {
+        CustomBotDatabaseManager.getCustomBotTokens().forEach(encryptedToken -> {
+            String token = Secret.decrypt(encryptedToken);
+            JDA jda = getCustomBotJda(token);
+            String botId = jda.getSelfUser().getId();
+            customJdaInstanceMap.put(botId, jda);
+            updateBotActivity(botId);
+        });
+    }
+
+    public void stopAll() {
+        customJdaInstanceMap.forEach((botId, jda) -> {
+            log.info("Shutting down {}", Utils.getJdaShardGuildCountString(jda));
+            jda.shutdown();
+        });
+        customJdaInstanceMap.clear();
+    }
+
+    public void updateBotActivity(String botId) {
+        JDA customBotJda = customJdaInstanceMap.getOrDefault(botId, null);
+        if (customBotJda == null) return;
+        Activity newActivity = CustomBotDatabaseManager.getCustomBotActivity(botId);
+        updateBotActivity(customBotJda, newActivity);
+    }
+
+    public void updateBotActivity(JDA jda, Activity newActivity) {
+        jda.getPresence().setActivity(newActivity);
+    }
+
+
+    @Override
+    public void shutdown() {
+        stopAll();
+    }
 }
