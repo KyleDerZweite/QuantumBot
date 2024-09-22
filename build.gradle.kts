d57973744bb874df68bf7e8bdaaf2f4a3e1de8f8
Kyle <lelegrau04@gmail.com>
2024-09-22 15:51:32 +0200

    ShadowJar added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
+    kotlin("jvm") version "1.8.0" // Use your Kotlin version
     id("java")
+    id("com.github.johnrengelman.shadow") version "8.1.0" // Use the latest version
 }
 
 group = "de.LuxuryBot"
@@ -10,6 +12,7 @@ repositories {
 }
 
 dependencies {
+    implementation(kotlin("stdlib"))
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
@@ -21,15 +24,22 @@ dependencies {
 
     implementation("net.dv8tion:JDA:5.1.0")
     implementation("com.google.code.gson:gson:2.11.0")
-
     implementation("net.oneandone.reflections8:reflections8:0.11.7")
-
     implementation("org.fusesource.jansi:jansi:2.4.0")
     implementation("org.slf4j:slf4j-api:2.0.5")                             // SLF4J API
     implementation("ch.qos.logback:logback-classic:1.5.6")                  // Logback Classic (with SLF4J binding)
     implementation("ch.qos.logback:logback-core:1.4.14")                    // Logback Core (required for configuration)
 
     implementation("org.postgresql:postgresql:42.7.2")
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    archiveBaseName.set("QuantumBot") // Set the base name for the JAR
+    archiveVersion.set("1.0.0") // Set the version
+    archiveClassifier.set("") // Remove the classifier
+    manifest {
+        attributes["Main-Class"] = "de.quantum.Main" // Specify your main class
+    }
 }
 
 tasks.withType<JavaCompile> {
